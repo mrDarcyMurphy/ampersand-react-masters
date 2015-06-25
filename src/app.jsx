4a8a5fd3b7f03317f943e113fa77c8b2ad6a8696
Darcy Murphy <mrDarcyMurphy@gmail.com>
2015-06-25 10:38:39 -0500

    for posterity

--- src/app.jsx
+++ src/app.jsx
@@ -1,3 +1,17 @@
+// ES6
+import React from 'react'
+import styles from './styles/main.css'
+
+const Hello = React.createClass({
+  render () {
+    return <div>Hello there, {this.props.name}.</div>
+  }
+})
+
+React.render(<Hello name="Darcy" />, document.body)
+
+
+// ES5
 var React = require('react');
 
 var Hello = React.createClass({
