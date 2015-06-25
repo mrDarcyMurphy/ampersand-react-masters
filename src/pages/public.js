fdb3b1c5f3ada33e936c4c560486f3e31b290839
Darcy Murphy <mrDarcyMurphy@gmail.com>
2015-06-25 10:38:49 -0500

    Added links between pages, because why not?

--- src/pages/public.js
+++ src/pages/public.js
@@ -3,7 +3,10 @@ import React from 'react'
 
 const PublicPage = React.createClass({
   render () {
-    return <div><h1>Public Page</h1></div>
+    return <div>
+      <h1>Public Page</h1>
+      <a href="/repos">/repos</a>
+    </div>
   }
 })
 
--- src/pages/repo.js
+++ src/pages/repo.js
@@ -3,7 +3,10 @@ import React from 'react'
 
 const RepoPage = React.createClass({
   render () {
-    return <div><h1>Repo Page</h1></div>
+    return <div>
+      <h1>Repo Page</h1>
+      <a href="/">/public</a>
+    </div>
   }
 })
 
